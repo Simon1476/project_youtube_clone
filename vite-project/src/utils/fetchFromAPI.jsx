bc6ff10d046a309db901a783c60b31b5822a5f02
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2023-07-29 19:42:47 +0900

    private

--- vite-project/src/utils/fetchFromAPI.jsx
+++ vite-project/src/utils/fetchFromAPI.jsx
@@ -7,7 +7,7 @@ const options = {
     maxResults: "50",
   },
   headers: {
-    "X-RapidAPI-Key": "9a0e1aa50bmsh8e0688802035b02p10877djsn562aa54529bf",
+    "X-RapidAPI-Key": "",
     "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
   },
 };
